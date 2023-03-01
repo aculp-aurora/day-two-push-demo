0974ee71248e4031016d8e1af08432088e1e5ce9
Alex Culp <aculp@aurorasolar.com>
2023-03-01 09:04:42 -0700

    initial commit, added some tests

---
+++ tests/cypress_tests/browser/smoke_tests_design_requests.ts
@@ -0,0 +1,3 @@
+// There is clearly a mstiake in this file
+// You will konw it because of the typos
+// There's only one per line so it souhld be easy to find
---
+++ tests/cypress_tests/browser/smoke_tests_designs.ts
@@ -0,0 +1,2 @@
+// Obvioulsy this would normally contain code
+// But what I want you to undertsnad is git, not javascrpit
