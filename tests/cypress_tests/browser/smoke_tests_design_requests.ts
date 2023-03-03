6c25dc2f4e7ef68b237fc4434b0290c5d4cfd677
Lilli Anderson <landerson@aurorasolar.com>
2023-03-03 13:36:03 -0500

    Lilli corrected files

--- tests/cypress_tests/browser/smoke_tests_design_requests.ts
+++ tests/cypress_tests/browser/smoke_tests_design_requests.ts
@@ -1,3 +1,3 @@
-// There is clearly a mstiake in this file
-// You will konw it because of the typos
-// There's only one per line so it souhld be easy to find
+// There is clearly a mistake in this file
+// You will know it because of the typos
+// There's only one per line so it should be easy to find
